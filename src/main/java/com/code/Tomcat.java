91240e6995566cc38e277f22d884f31a5a8c3890
鲸落 <10916168+whale-fall_study@user.noreply.gitee.com>
2023-11-12 22:20:56 +0800

    initialization

---
+++ src/main/java/com/code/Tomcat.java
@@ -0,0 +1,13 @@
+package com.code;
+
+public class Tomcat {
+
+    public void start() {
+
+    }
+
+    public static void main(String[] args) {
+        Tomcat tomcat = new Tomcat();
+        tomcat.start();
+    }
+}
